26aeabed82aed7e6162792509a75f537e032fa65
Nayan Dave <nayan.dave@sportzinteractive.net>
2024-04-18 12:26:38 +0530

    Initial Commit

---
+++ Learning_SwiftUI_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning_SwiftUI_1
+//
+//  Created by Nayan Dave on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning_SwiftUI_1/Learning_SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUI_1App.swift
+//  Learning_SwiftUI_1
+//
+//  Created by Nayan Dave on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
